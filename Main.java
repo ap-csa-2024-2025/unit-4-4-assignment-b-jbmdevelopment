bfe601809b1bd6c18ea6646092b3b1b0288a029d
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-14 18:53:53 +0000

    a

--- Main.java
+++ Main.java
@@ -4,7 +4,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Put your solutions below
+    // problem 
+    Scanner input = new Scanner(System.in);
+    int counter = 0;
+    System.out.println("Enter a sentence");
+    String sentence = input.nextLine();
+    System.out.println("Enter a target (word) to find in the sentence");
+    String target = input.nextLine();
+    for(int i = 0; i < sentence.length() - target.length() - 1; i++) {
+        String str = sentence.substring(i, i + target.length());
+        if(str.equals(target)) {
+            counter++;
+        }
+    }
+    System.out.println(target + " appears " + counter + " times");
+    
   }
 
   public static int countSub(String word, String target)
