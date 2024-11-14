8c2b0b2ca533a9ab99ef4db59ad37a8c5dbb3425
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-14 19:00:05 +0000

    ab

--- Main.java
+++ Main.java
@@ -6,24 +6,25 @@ public class Main
   {
     // problem 
     Scanner input = new Scanner(System.in);
-    int counter = 0;
     System.out.println("Enter a sentence");
     String sentence = input.nextLine();
     System.out.println("Enter a target (word) to find in the sentence");
     String target = input.nextLine();
-    for(int i = 0; i < sentence.length() - target.length() - 1; i++) {
-        String str = sentence.substring(i, i + target.length());
-        if(str.equals(target)) {
-            counter++;
-        }
-    }
+    int counter = countSub(sentence, target);
     System.out.println(target + " appears " + counter + " times");
-    
+
   }
 
   public static int countSub(String word, String target)
   {
-    return 0;
+  int counter = 0;
+  for(int i = 0; i < word.length() - target.length() - 1; i++) {
+      String str = word.substring(i, i + target.length());
+      if(str.equals(target)) {
+            counter++;
+       }
+    }
+    return counter;
   }
 
   public static int countProperContains(String word, String target)
